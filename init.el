9efc6cbd4363571b2e778b7ddc81986936c593a2
John Downs <john.downs@acm.org>
2017-10-02 22:17:58 -0400

    set racket path

--- init.el
+++ init.el
@@ -1,1 +1,34 @@
+;;; Sensible color theme
 (load-theme 'tango-dark)
+
+;;; Set up melpa
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.org/packages/") t)
+
+(setenv "PATH"
+	(concat (getenv "PATH")
+		":" "/usr/local/bin"))
+
+;;; Installed Packages
+
+;; Magit
+;; Geiser
+(setq geiser-racket-binary "/Applications/Racket v6.0/bin/racket")
+;; Paredit
+
+
+
+;;; Customizations
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(scheme-program-name "/usr/local/bin/racket"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
